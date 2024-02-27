47aea3c33f20c6d55660c25ebcfe07e20e7ea605
Valerie <kekvalery@gmail.com>
2024-02-28 02:17:53 +0300

    chore: add all required components to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,19 @@
-"use client";
-
 import Image from "next/image";
 import styles from "./page.module.css";
-import { getIds, getItems, getFields, filter } from "@/api/api";
-import { SetStateAction, useState } from "react";
+import { getIds, getItems, getFields, filter } from "@/api";
+import ProductList from "@/components/ProductList";
+import { getFirstRenderProductsList } from "@/utils/helpers";
+import Filter from "@/components/Filter/Filter";
+import Pagination from "@/components/Pagination/Pagination";
 
-export default function Home() {
-  const [data, setData] = useState("ничего");
-
-  async function onButClick() {
-    // const res: SetStateAction<string> = await getIds({offset: 3, limit: 4});
-    // const res: SetStateAction<string> = await getItems({ids: ["1789ecf3-f81c-4f49-ada2-83804dcc74b0"]});
-    // const res: SetStateAction<string> = await filter({price: 17500.0});
-    const res: SetStateAction<string> = await getFields({"field": "price", "offset": 1, "limit": 5});
-    setData(JSON.stringify(res));
-  }
+export default async function Home() {
+  const firstRenderProductsList = await getFirstRenderProductsList();
 
   return (
     <>
-      <div>{data}</div>
-      <button onClick={onButClick}>оп</button>
+      <Filter />
+      <ProductList firstRenderProductsList={firstRenderProductsList} />
+      <Pagination/>
     </>
   );
 }
