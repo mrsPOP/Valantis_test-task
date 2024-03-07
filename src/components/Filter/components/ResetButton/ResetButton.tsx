7f3226896523689d6d56719e49e54f8446c9724d
Valerie <kekvalery@gmail.com>
2024-03-07 21:00:04 +0300

    fix: use preventDefault() for resetButton

--- src/components/Filter/components/ResetButton/ResetButton.tsx
+++ src/components/Filter/components/ResetButton/ResetButton.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { resetFilteredPages } from "@/lib/features/filteredPages/filteredPagesSlice";
 import { resetFilter } from "@/lib/features/filter/filterSlice";
-import { useAppDispatch } from "@/lib/hooks";
+import { resetFilteredPages } from "@/lib/features/filteredPages/filteredPagesSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { SyntheticEvent } from "react";
 
 const ResetButton = () => {
+  const filters = useAppSelector((state) => state.filter.filter);
   const dispatch = useAppDispatch();
-  const OnClick = () => {
-    dispatch(resetFilter());
-    dispatch(resetFilteredPages());
-  }
+  const filtersIsOn = Object.keys(filters).length > 0;
+
+  const OnClick = (event: SyntheticEvent) => {
+    event.preventDefault();
+    if (filtersIsOn) {
+      dispatch(resetFilter());
+      dispatch(resetFilteredPages());
+    }
+  };
   return <button onClick={OnClick}>Сбросить фильтры</button>;
 };
 
