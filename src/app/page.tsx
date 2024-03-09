01cc3f92d59dba076bc1a2f408cf5033039e3193
Valerie <kekvalery@gmail.com>
2024-03-09 21:03:24 +0300

    chore: remove extra imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { getIds, getItems, getFields, filter } from "@/api";
-import ProductList from "@/components/ProductList";
-import { getProductsList } from "@/utils/helpers";
 import Filter from "@/components/Filter";
 import Pagination from "@/components/Pagination";
-import StoreProvider from "./StoreProvider";
+import ProductList from "@/components/ProductList";
 import { productsNumberOnPage } from "@/utils/constatns";
-import { getDataForFiltersSelects } from "@/utils/helpers";
+import { getDataForFiltersSelects, getProductsList } from "@/utils/helpers";
+import StoreProvider from "./StoreProvider";
 
 export default async function Home() {
   const firstRenderProductsList = await getProductsList({
